ebfdaaa02c5dfdedec2edf226b910194988e8b58
SergeyArenzon <sergeyr1991@gmail.com>
2021-02-15 14:27:43 +0200

    add hebrew -> gregorian

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 import axios from "axios";
+import { ReactJewishDatePicker, BasicJewishDay } from "react-jewish-datepicker";
 
 function App() {
-    const [isHebrew, setIsHenrew] = useState(true);
+    const [toHebrew, setToHebrew] = useState(true);
     const [date, setDate] = useState("");
     const [output, setOutput] = useState("");
     const [error, setError] = useState("");
     const [afterSunshine, setAfterSunshine] = useState("");
+    const [basicJewishDay, setBasicJewishDay] = useState("");
 
     const inputDateHandler = (inputDate) => {
         const date = {};
@@ -19,39 +20,82 @@ function App() {
         return date;
     };
 
+    const inputHebrewDateHandler = (inputDate) => {
+        const date = {};
+        date.month = inputDate.slice(4, 7);
+        date.day = inputDate.slice(8, 10);
+        date.year = inputDate.slice(11, 15);
+        return date;
+    };
     const convertHandler = () => {
         if (date !== "") {
-            setError('');
-            const fixedDate = inputDateHandler(date);
+            setError("");
 
-            axios
-                .get(
-                    "https://www.hebcal.com/converter?cfg=json&gy=" +
-                        fixedDate.year +
-                        "&gm=" +
-                        fixedDate.month +
-                        "&gd=" +
-                        fixedDate.day +
-                        "&g2h=1"
-                )
-                .then((res) => setOutput(res.data.hebrew));
+            if (toHebrew) {
+                //  gregorian --> hebrew mode
+                const fixedDate = inputDateHandler(date);
+
+                axios
+                    .get(
+                        "https://www.hebcal.com/converter?cfg=json&gy=" +
+                            fixedDate.year +
+                            "&gm=" +
+                            fixedDate.month +
+                            "&gd=" +
+                            fixedDate.day +
+                            "&g2h=1"
+                    )
+                    .then((res) => setOutput(res.data.hebrew));
+            }
+        } else if (basicJewishDay) {
+            const fixedDate = inputHebrewDateHandler(basicJewishDay.date.toString());
+            setOutput(fixedDate.day + ' ' + fixedDate.month + ' ' + fixedDate.year)
         } else {
-
-            setError('NO CORRECT DATE FORMAT CHOSEN!');
+            setError("NO CORRECT DATE FORMAT CHOSEN!");
         }
     };
+
+
+    // handles change convert modes
+    const changeHandler = () => {
+        setToHebrew(!toHebrew);
+        setOutput('');
+        setError('');
+    };
+
+    let format = (
+        <div>
+            <input
+                type="date"
+                onChange={(event) => setDate(event.target.value)}
+            ></input>
+        </div>
+    );
+
+    if (!toHebrew) {
+        format = (
+            <ReactJewishDatePicker
+                value={new Date()}
+                isHebrew
+                onClick={(day) => {
+                    setBasicJewishDay(day);
+                }}
+            />
+        );
+    }
 
     return (
         <div className="App">
             <h1>Convert your date</h1>
             {error}
-            <input
-                type="date"
-                onChange={(event) => setDate(event.target.value)}
-            ></input>
+            {format}
             <button onClick={convertHandler}>Convert</button>
             <div>{output}</div>
             <div>{date}</div>
+            <button onClick={changeHandler}>
+                {" "}
+                {toHebrew ? "Jewish TO Gregorain" : "Gregorain TO Jewish"}
+            </button>
         </div>
     );
 }
